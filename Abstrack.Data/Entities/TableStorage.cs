b6fbbd1b629fb294bce5e35564c366fc4e49b177
Nick Brooks <nbrooks@thirtyseven.com.au>
2017-12-27 16:16:36 +1030

    TableStorage and Cosmos

---
+++ Abstrack.Data/Entities/TableStorage.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+
+namespace Abstrack.Data.Entities
+{
+    public class RequestKey : TableEntity
+    {
+        public RequestKey(string trackId, string requestKey)
+        {
+            PartitionKey = "track_request_key";
+            RowKey = requestKey;
+            this.track_id = trackId;
+        }
+
+        public RequestKey() { }
+
+        public string track_id { get; set; }
+    }
+}
---
+++ Abstrack.Data/Entities/Track.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Abstrack.Data.Entities
+{
+    public class Track
+    {
+        public string id { get; set; }
+        public string owner_id { get; set; }
+        public DateTime date_created { get; set; }
+        public byte[] request_key { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+
+        public Track()
+        {
+            id = new Guid().ToString();
+            date_created = DateTime.UtcNow;
+
+            using (SHA512 shaM = new SHA512Managed())
+            {
+                request_key = shaM.ComputeHash(Encoding.UTF8.GetBytes(id + date_created.ToString()));
+            }
+        }
+    }
+}
