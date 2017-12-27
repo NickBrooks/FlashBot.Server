f78a7c41fc7dad016b8c85676504491304cfc7ea
Nick Brooks <nbrooks@thirtyseven.com.au>
2017-12-27 15:02:24 +1030

    Tracks page

--- Abstrack.Admin/Pages/Account/Manage/ManageNavPages.cs
+++ Abstrack.Admin/Pages/Account/Manage/ManageNavPages.cs
@@ -10,6 +10,8 @@ namespace Abstrack.Admin.Pages.Account.Manage
     {
         public static string Index => "Index";
 
+        public static string Tracks => "Tracks";
+
         public static string ChangePassword => "ChangePassword";
 
         public static string ExternalLogins => "ExternalLogins";
@@ -17,6 +19,8 @@ namespace Abstrack.Admin.Pages.Account.Manage
         public static string TwoFactorAuthentication => "TwoFactorAuthentication";
 
         public static string IndexNavClass(ViewContext viewContext) => PageNavClass(viewContext, Index);
+
+        public static string TracksNavClass(ViewContext viewContext) => PageNavClass(viewContext, Tracks);
 
         public static string ChangePasswordNavClass(ViewContext viewContext) => PageNavClass(viewContext, ChangePassword);
 
