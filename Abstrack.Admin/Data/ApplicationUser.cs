e883dbad4a51778a5b4ae828048fbd315c222bbe
Nick Brooks <nbrooks@thirtyseven.com.au>
2018-04-05 09:48:40 +1000

    Updated application user

--- Abstrack.Admin/Data/ApplicationUser.cs
+++ Abstrack.Admin/Data/ApplicationUser.cs
@@ -5,5 +5,15 @@ namespace Abstrack.Admin.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public int Public_Tracks { get; set; }
+        public int Private_Tracks { get; set; }
+        public int Rate_Per_Track { get; set; }
+        public int Max_Track_Storage { get; set; }
+        public Subscription Subscription { get; set; }
+    }
+
+    public enum Subscription
+    {
+        Free
     }
 }
